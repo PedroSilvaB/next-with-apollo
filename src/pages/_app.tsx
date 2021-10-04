7efcf43bc03fc3f10821461f254b18054d6f248c
Pedro Silva <55249637+PedroSilvaB@users.noreply.github.com>
2021-10-04 18:30:04 -0300

    added Apollo, Graphql and SWR

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import "../styles/globals.css";
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <ApolloProvider client={client}>
+      {getLayout(<Component {...pageProps} />)}
+    </ApolloProvider>
+  );
+}
